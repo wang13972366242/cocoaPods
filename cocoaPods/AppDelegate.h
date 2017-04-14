bb24342dd0624d5f0a03ddaac732623423420c91
wang13972366242 <435840885@qq.com>
2017-04-14 16:00:56 +0800

    Initial Commit

---
+++ cocoaPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cocoaPods
+//
+//  Created by wang on 14/04/2017.
+//  Copyright © 2017 person. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cocoaPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cocoaPods
+//
+//  Created by wang on 14/04/2017.
+//  Copyright © 2017 person. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
